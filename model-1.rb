b9a1e2401f88f367172cd9fcf6b1d89819276f7b
Tamara Dowis <tamara.dowis@gmail.com>
2021-09-23 20:07:08 -0500

    Add booleans

--- model-1.rb
+++ model-1.rb
@@ -3,3 +3,7 @@ car = "I hope it works out tomorrow."
 son = "Tenzin is amazing!"
 floats = 1.2, 5.90, 472.0
 integers = 1,8,608456
+booleans
+am_I_tired = yes
+is_she_cuddly = yes
+is_food_good = no
